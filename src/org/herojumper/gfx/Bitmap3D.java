a4c16a00f188624e395fa0732d199dd355be0b3a
Retr0A <92212390+Retr0A@users.noreply.github.com>
2021-12-27 15:32:19 +0200

    More texture configuration
    Addition to "Added Textures" commit.

--- src/org/herojumper/gfx/Bitmap3D.java
+++ src/org/herojumper/gfx/Bitmap3D.java
@@ -26,7 +26,7 @@ public class Bitmap3D extends Bitmap {
 		//yCam = t / 100.0;
 		//zCam = Math.sin(t / 300.0);
 		
-		rot = t / 1000.0;
+		//rot = t / 1000.0;
 		
 		double rSin = Math.sin(rot);
 		double rCos = Math.cos(rot);
@@ -47,8 +47,23 @@ public class Bitmap3D extends Bitmap {
 				
 				depthBuffer[x + y * width] = zd;
 				pixels[x + y * width] = Textures.tilemap.pixels[(xPix & 15) | (yPix & 15) * Textures.tilemap.width];
+				
+				drawFloor(xd, zd, yd, x, y, xPix, yPix, 0, 2);
+				drawFloor(xd, zd, yd, x, y, xPix, yPix, 0, 3);
+				drawFloor(xd, zd, yd, x, y, xPix, yPix, 1, 2);
+				drawFloor(xd, zd, yd, x, y, xPix, yPix, 1, 3);
 			}
 		}
+	}
+	
+	public void drawFloor(double xd, double zd, double yd, int x, int y, int xPix, int yPix, int pX, int pY) {
+		if (yd >=0 && xd >= pX * 16 && xd < pX * 16 + 16 && zd >= pY * 16 && zd < pY * 16 + 16)
+			pixels[x + y * width] = Textures.tilemap.pixels[(xPix & 15) + 16 | (yPix & 15) * Textures.tilemap.width];
+	}
+	
+	public void drawCeiling(double xd, double zd, double yd, int x, int y, int xPix, int yPix, int pX, int pY) {
+		if (yd <=0 && xd >= pX * 16 && xd < pX * 16 + 16 && zd >= pY * 16 && zd < pY * 16 + 16)
+			pixels[x + y * width] = Textures.tilemap.pixels[(xPix & 15) + 16 | (yPix & 15) * Textures.tilemap.width];
 	}
 	
 	public void renderFog() {
